789b9907dc2055e6c24be411b1052f545249a804
mc1920 <mc1920@ic.ac.uk>
2026-08-12 21:29:14 +0900

    8/12 hw check

--- Due_8_12/BaggageManager.java
+++ Due_8_12/BaggageManager.java
@@ -25,14 +25,33 @@ public class BaggageManager
 
     public double calculateTotalWeight(String flightCode)
     {
-        // TO COMPLETE
-        return 0;
+        double total = 0.0;
+        for (int i = 0; i < numberOfBags; i++)
+        {
+            if (allBaggage[i].getFlight().getFlightCode().equals(flightCode))
+            {
+                total += allBaggage[i].getWeight();
+            }
+        }
+
+        return total;
     }
 
     public Baggage[] getOverweightBaggage()
     {
-        // TO COMPLETE
-        return null;
+        Baggage[] overweight = new Baggage[200];
+        int count = 0;
+
+
+        for (int i = 0; i < numberOfBags; i++)
+        {
+            if (allBaggage[i].getWeight() > allBaggage[i].getFlight().getMaximumWeight())
+            {
+                overweight[count] = allBaggage[i];
+                count++;
+            }
+        }
+        return overweight;
     }
 
     private void swap(int x, int y)
@@ -47,7 +66,13 @@ public class BaggageManager
 
     public int countUnloaded(int index)
     {
-        // TO COMPLETE
-        return 0;
+        if (index >= numberOfBags) {
+            return 0;
+        }
+        if (!allBaggage[index].isLoaded()) {
+            return 1 + countUnloaded(index + 1);
+        }
+
+        return countUnloaded(index + 1);
     }
 }
--- Due_8_12/DelayManager.java
+++ Due_8_12/DelayManager.java
@@ -13,18 +13,49 @@ public class DelayManager
 
     public void insertByDelay(DelayedBag newBag)
     {
-        // TO COMPLETE
+        int index = 0;
+        while (index < delayedBags.size() && delayedBags.get(index).getDelayMinutes()
+                <= newBag.getDelayMinutes()) {
+            index++;
+        }
+
+        delayedBags.add(index, newBag);
     }
 
+    // 10 20 35 60 90
+    
     public DelayedBag removeLongestDelay()
     {
-        // TO COMPLETE
-        return null;
+        if (delayedBags.size() == 0) {
+            return null;
+        }
+
+        return delayedBags.remove(delayedBags.size() - 1);
     }
 
+    /*
+
+    if (index >= numberOfBags) {
+            return 0;
+        }
+        if (!allBaggage[index].isLoaded()) {
+            return 1 + countUnloaded(index + 1);
+        }
+
+        return countUnloaded(index + 1);
+
+
+     */
     public int countSevereDelays(int index)
     {
-        // TO COMPLETE
-        return 0;
+        if (index >= delayedBags.size()) {
+            return 0;
+        }
+
+        if (delayedBags.get(index).getDelayMinutes() > 60) {
+            return 1 + countSevereDelays(index + 1);
+        }
+
+        return countSevereDelays(index + 1);
     }
 }
